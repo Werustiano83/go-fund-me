3d1df6c7853e6eb4441d5480dc0116e946c2791b
Raul Villareal <rvillarrcal@cnm.edu>
2016-07-15 11:12:44 -0600

    fixed interpreter

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
 	<body>
 		<h1> gofundme.com Data Design Analysis </h1>
 		<h2> Persona </h2>
+		<p> Mrs. Generous has a big heart; since she's a widow and her "God Rest His Soul" husband left her in a very wealthy financial shape, she constantly looks for opportunities to give someone a financial lift. </p>
+		<p> A couple of days ago she was at a church meeting and found out that the Church Choir was planning a field trip, and they were using gofundme.com to raise funds.</p>
 		<h2> Use Case</h2>
 		<h2> Interaction Flow </h2>
 		<h2> Conceptual Model </h2>
