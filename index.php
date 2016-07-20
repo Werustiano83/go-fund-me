c49e668586504eb80aea13be793e56313ad5d129
Raul Villareal <rvillarrcal@cnm.edu>
2016-07-20 09:43:20 -0600

    created SQL folder and file

--- index.php
+++ index.php
@@ -1,16 +1,49 @@
 <!DOCTYPE html>
 <html lang="en">
+	<title> GOFUNDME Data </title>
 	<head>
 		<meta charset="utf-8"/>
+		<link href="css/style.css" rel="stylesheet" type="text/css"/>
+		<link href='https://fonts.googleapis.com/css?family=Montserrat:700' rel='stylesheet' type='text/css'>
 		<title>Basic HTML Page</title>
 	</head>
-	<body>
-		<h1> gofundme.com Data Design Analysis </h1>
-		<h2> Persona </h2>
-		<p> Mrs. Generous has a big heart; since she's a widow and her "God Rest His Soul" husband left her in a very wealthy financial shape, she constantly looks for opportunities to give someone a financial lift. </p>
-		<p> A couple of days ago she was at a church meeting and found out that the Church Choir was planning a field trip, and they were using gofundme.com to raise funds.</p>
-		<h2> Use Case</h2>
-		<h2> Interaction Flow </h2>
-		<h2> Conceptual Model </h2>
-	</body>
+		<body>
+			<div class="stitched">
+				<h1> gofundme.com Data Design Analysis </h1>
+			</div>
+			<h2> Persona </h2>
+				<p> Mrs. Gene Orus has a big heart; since she's a widow and her "God Rest His Soul" husband left her in a very wealthy financial shape, she constantly looks for opportunities to give someone a financial lift. </p>
+				<p> A couple of days ago she was at a church meeting and found out that the Church Choir was planning a field trip, and they were using gofundme.com to raise funds.</p>
+				<p> Since she's very shy and someone told her that she could help anonymously, she decided to give it a try... she's not very tech savvy though. This is what she needs to donate money online:</p>
+
+			<h2> Use Case</h2>
+				<p>Mrs. Orus  becomes a fan of gofundme.com as well as other crowfunding sites. She gets into the habit of doing a little research for causes that she wants to support. Once a week she chooses that one cause where she can make a real difference anonimously.</p>
+				<ol>
+					<li> Her main purpose is to make a real difference to people in need.</li>
+					<li> She does it because life has been good with her  and she wants to practice some gratitude. </li>
+					<li> She decides to choose a cause and donate once a week.</li>
+					<li> She doesn't have a computer at home, so she goes to the public library to use their desktops, sometimes she uses a desktop at her church. </li>
+					<li> The library desktops are all equipped with Windows 8 and an internet connection that has decent speed. The desktop at church, has Windows XP, the internet coonnection at church fails a lot. </li>
+
+				</ol>
+			<h2> Interaction Flow </h2>
+				<ol>
+					<li> Get into the website</li>
+					<li> Locate the Church Choir campaign </li>
+					<li> Locate the donation option </li>
+					<li> Click on such option to donate</li>
+					<li> Enter donation amount </li>
+					<li> Enter her bank information account</li>
+					<li> Submit the donation </li>
+					<li> Be able to exit from the site securely </li>
+				</ol>
+
+			<h2> Conceptual Model </h2>
+				<ul>
+					<li>each user can create <strong>many</strong> campaigns</li>
+					<li>each user can donate to <strong>many</strong> campaigns</li>
+					<li>each user can donate <strong>many times</strong> to the same campaign</li>
+					<li>each campaign can accept <strong>many</strong> donations</li>
+				</ul>
+		</body>
 </html>
